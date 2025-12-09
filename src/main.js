b7199381c72565979a4b0282f4dd9fd489c48625
monkeysh1ne <mcdoob@gmail.com>
2025-12-09 13:10:04 +1000

    adding auth0 to netlify setup

---
+++ src/main.js
@@ -0,0 +1,26 @@
+import { useAuth0 } from '@auth0/auth0-react'
+
+function App() {
+  const { loginWithRedirect, logout, isAuthenticated, user } = useAuth0()
+
+  return (
+    <>
+      {!isAuthenticated && <button onClick={loginWithRedirect}>Login</button>}
+
+      {isAuthenticated && (
+        <>
+          <p>Hello, {user.name}</p>
+          <button
+            onClick={() =>
+              logout({ logoutParams: { returnTo: window.location.origin } })
+            }
+          >
+            Logout
+          </button>
+        </>
+      )}
+    </>
+  )
+}
+
+export default App
